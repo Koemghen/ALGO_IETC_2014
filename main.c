ba5e92a3dae107e5dfcc0a9a75ae7ab5d37d5d51
Marion11 <marionhancart@yahoo.fr>
2014-12-03 20:38:28 +0100

    verifprenom

--- main.c
+++ main.c
@@ -17,7 +17,29 @@ int verifnom (char* chaine)
              gets (chaine);         
         }
         
-        else if (chaine[i]>=65 && chaine[i]>=90 || chaine[i]==32)
+        else if (chaine[i]>=65 && chaine[i]<=90 || chaine[i]==32)
+        {
+             return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        i++;
+    }
+}
+
+int verifprenom (char* chaine)
+{
+    int i=0;
+    while (chaine[i]!='\0')
+    { 
+        if(chaine[0]==32||chaine[0]==45)
+        {
+              printf("Erreur! Votre pr%cnom ne peut commencer par un espace ni par un tiret! R%cintroduisez votre nom:\n",130,130);
+              gets(chaine);
+        }           
+        else if((chaine[i]>=65 && chaine[i]<=90) || chaine[i]==32 || chaine[i]==45)
         {
              return 1;
         }
