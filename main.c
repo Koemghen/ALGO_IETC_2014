2c8f169974a01802642a193ce3882c13591db447
Joannedumortier <Joannedumortier@users.noreply.github.com>
2014-12-03 20:27:50 +0100

    Fonction Verifnom

--- main.c
+++ main.c
@@ -5,3 +5,26 @@ void main(void)
 {
  return 0;
 }
+
+int verifnom (char* chaine)
+{
+    int i=0;
+    while (chaine[i] != '\0')
+    {
+        if (chaine[0] == 32)
+        {
+             printf("Erreur! Votre nom ne peut commencer par un espace! Veuillez r%cintroduire votre nom: ",130);
+             gets (chaine);         
+        }
+        
+        else if (chaine[i]>=65 && chaine[i]>=90 || chaine[i]==32)
+        {
+             return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        i++;
+    }
+}
